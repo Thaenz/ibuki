440651138141c98ee2fba14931be7b04d5560c5f
Thaenz <thaenz@pm.me>
2024-05-28 09:58:46 +0200

    http -> https

--- ibuki/patches/suckless.scm
+++ ibuki/patches/suckless.scm
@@ -6,7 +6,7 @@
   (let ((version "0.8.5"))
     (origin
       (method url-fetch)
-      (uri (string-append "http://st.suckless.org/patches/gruvbox"
+      (uri (string-append "https://st.suckless.org/patches/gruvbox"
 			  "/st-gruvbox-dark-" version ".diff"))
       (sha256
 	(base32 "0jvn0i0fl0w3c8dcmwyh9w19g3hsi22cqmyqly5zjzjwjhc8qnjv")))))
