8ab1be222fe101adba7a7cc184d9df67b09a425d
Thaenz <thaenz@pm.me>
2024-08-05 13:32:54 +0200

    add dmenu-ibuki

--- ibuki/packages/suckless.scm
+++ ibuki/packages/suckless.scm
@@ -21,6 +21,28 @@
         (base32 "0js9z5kn8hmpxzfmb2g6zsy28zkpg88j3wih5wixc89b8x7ms8bb"))
        (patches (list st-gruvbox-dark))))))
 
+(define-public dmenu-ibuki
+  (package
+    (inherit dmenu)
+    (name "dmenu-ibuki")
+    (version "5.3")
+    (source
+      (origin
+	(method url-fetch)
+	(uri (string-append "https://dl.suckless.org/tools/dmenu-"
+			    version ".tar.gz"))
+       (sha256
+	 (base32 "0pvr6da1v7hmbnacpgxcxv1sakg1nckmw347xhwrhx1dzpk573qs"))
+       (modules '((guix build utils)))
+       (snippet '(substitute* "config.def.h"
+			      (("lines      = 0") "lines      = 20")
+			      ;; fg                    bg
+			      (("#bbbbbb") "#ebdbb2") (("#222222") "#282828")
+			      (("#eeeeee") "#282828") (("#005577") "#fabd2f")
+			      (("#000000") "#282828") (("#00ffff") "#98971a")))
+       (patches (list dmenu-center
+		      dmenu-border))))))
+
 (define-public dwm-ibuki
   (package
     (inherit dwm)
--- ibuki/patches/suckless.scm
+++ ibuki/patches/suckless.scm
@@ -10,3 +10,29 @@
 			  "/st-gruvbox-dark-" version ".diff"))
       (sha256
 	(base32 "0jvn0i0fl0w3c8dcmwyh9w19g3hsi22cqmyqly5zjzjwjhc8qnjv")))))
+
+(define-public dmenu-center
+  (let ((version "5.2"))
+    (origin
+      (method url-fetch)
+      (uri (string-append "https://tools.suckless.org/dmenu/patches"
+			  "/center/dmenu-center-" version ".diff"))
+      (sha256
+	(base32 "1jck88ypx83b73i0ys7f6mqikswgd2ab5q0dfvs327gsz11jqyws"))
+      (file-name "dmenu-center.diff")
+      (modules '((guix build utils)))
+      (snippet '(substitute* "dmenu-center.diff"
+			     (("centered = 0") "centered = 1"))))))
+
+(define-public dmenu-border
+  (let ((version "20230512-0fe460d"))
+    (origin
+      (method url-fetch)
+      (uri (string-append "https://tools.suckless.org/dmenu/patches"
+			  "/border/dmenu-border-" version ".diff"))
+      (sha256
+	(base32 "12lahhbp9nwkzlbv4nbmvwb90sgnci93gspzq1cyspj21pd7azw5"))
+      (file-name "dmenu-border.diff")
+      (modules '((guix build utils)))
+      (snippet '(substitute* "dmenu-border.diff"
+			     (("border_width = 0") "border_width = 3"))))))
